0a66af8a1d50a1473aa70d349ab74ffe7fbac0b7
Jesse Boucher <jesseboucher8@gmail.com>
2017-05-30 19:37:04 -0600

    finished mini-angular project

---
+++ js/mainCtrl.js
@@ -0,0 +1,25 @@
+angular.module('friendsList').controller('mainCtrl', function($scope) {
+	// console.log('connected')
+	// $scope array of friends
+
+	$scope.myFriends = ['Nate', 'Seth', 'Manuel', 'Gina'];
+
+	// $scope.myFriends = [
+	// 	{
+	// 		name: 'Nate',
+	// 		age: 38
+	// 	},
+	// 	{
+	// 		name: 'Seth',
+	// 		age: 40
+	// 	},
+	// 	{
+	// 		name: 'Gina',
+	// 		age: 38
+	// 	},
+	// 	{
+	// 		name: 'Colby',
+	// 		age: 17
+	// 	}
+	// ]
+})
